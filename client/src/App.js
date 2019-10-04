b3ffd952c7106b51c760dbb2d7d26cf1c4c87550
Sydney Blom <sydneyblom@#gmail.com>
2019-10-04 11:36:53 -0500

    finished fetching data and got it working

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import axios from 'axios';
+import PlayerCard from './components/PlayerCard';
+
+class App extends React.Component {
+  constructor() { 
+    super();
+  this.state = {
+    players: [],
+  };  
 }
+
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/api/players`)
+      .then(res => {
+        this.setState({
+          players: res.data
+        })
+      })
+      .catch(err => alert(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+     {this.state.players.map(player => {
+       return (
+        <PlayerCard name={player.name} country={player.country} searches={player.searches} />
+  
+       )
+     })} 
+</div>
+    );
+  }
+} 
+ 
 
 export default App;
--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -1,1 +1,19 @@
 import React from 'react';
+import { Card } from 'semantic-ui-react';
+
+
+function PlayerCard({name, country, searches}) {
+
+      return (
+        <Card>
+            <Card.Content>
+            <Card.Header>{name}</Card.Header>
+            <Card.Meta>{country}</Card.Meta>
+            <Card.Description>{searches}</Card.Description>
+            </Card.Content>
+        </Card>
+    );
+}
+
+
+export default PlayerCard;
